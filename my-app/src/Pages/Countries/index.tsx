8038d44cabac1e75826c2667ce19e13280168223
victor misiko <victormisiko.vm@gmail.com>
2022-10-15 05:32:00 +0300

    Update: Add Page Structure

---
+++ my-app/src/Pages/Countries/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Countries() {
+  return (
+    <div>
+      <span>countries</span>
+    </div>
+  )
+};
+
+export default Countries;
---
+++ my-app/src/Pages/CountryDetail/index.tsx
@@ -0,0 +1,11 @@
+
+
+function CountryDetail() {
+  return (
+    <div>
+      <span>country</span>
+    </div>
+  )
+};
+
+export default CountryDetail;
