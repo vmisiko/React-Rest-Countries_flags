ea698c7cfc09ac900445650d62a4081b2ddfe49a
victor misiko <victormisiko.vm@gmail.com>
2022-10-15 20:46:44 +0300

    TEST: update tests for countries page with fetching all countries, filter by country and filter by region tests

--- my-app/src/Tests/Countries.cy.tsx
+++ my-app/src/Tests/Countries.cy.tsx
@@ -1,8 +1,38 @@
 import Countries from "../Pages/Countries";
 
 describe("<Countries>", () => {
-  it('Mounted Countries', () => {
+  beforeEach(() => {
     cy.mount(<Countries />);
+  });
+  it('Mounted Countries', () => {
     cy.contains('countries');
-  })
+  });
+  it('Fetch all countries', () => {
+    cy.request({
+      method: 'GET',
+      url: 'https://restcountries.com/v3.1/all',
+    }).then(($response) => {
+      expect($response.status).to.eq(200);
+    });
+  });
+
+  it('Filter By country', () => {
+    const country = 'kenya';
+    cy.request({
+      method: 'GET',
+      url: `https://restcountries.com/v3.1/name/${country}`,
+    }).then(($response) => {
+      expect($response.status).to.eq(200);
+    });
+  });
+
+  it('Filter By Region', () => {
+    const region = "AU"
+    cy.request({
+      method: 'GET',
+      url: `https://restcountries.com/v3.1/regionalbloc/{region}`,
+    }).then(($response) => {
+      expect($response.status).to.eq(200);
+    });
+  });
 });
