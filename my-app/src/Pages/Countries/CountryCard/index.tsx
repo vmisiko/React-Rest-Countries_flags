804b22e044e7b3ca3f573175d91f0cc7c3669c19
victor misiko <victormisiko.vm@gmail.com>
2022-10-15 20:57:41 +0300

    Update: Add styling for CountryCard Component for visualising country flags and country name

--- my-app/src/Pages/Countries/CountryCard/index.tsx
+++ my-app/src/Pages/Countries/CountryCard/index.tsx
@@ -1,9 +1,22 @@
+import './index.scss';
 
-
-function countryCard(props:{country: unknown}) {
+function countryCard(props:{country: any}) {
   return (
-    <div>
-      <span>countries</span>
+    <div id="card" className="country-card">
+      <div className="country-card__image">
+        <img src={props.country.flags.svg} alt="" />
+      </div>
+      <div className="country-card__content">
+        <h2 className="name">
+          {props.country.name.official}
+        </h2>
+
+        <ol className="list">
+          <li> Population: <span>{props.country.population}</span></li>
+          <li> Region: <span>{props.country.region}</span></li>
+          <li> Capital: <span>{props.country?.capital}</span></li>
+        </ol>
+      </div>
     </div>
   )
 };
