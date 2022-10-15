33b4809cf7c720654df3b5ec046aac8d3f08ade7
victor misiko <victormisiko.vm@gmail.com>
2022-10-15 21:36:44 +0300

    Test: Add test blue print for CountryDetails Page.

--- my-app/src/Tests/CountryDetail.cy.tsx
+++ my-app/src/Tests/CountryDetail.cy.tsx
@@ -1,8 +1,37 @@
 import CountryDetail from "../Pages/CountryDetail";
 
-describe("<Countries>", () => {
-  it('Mounted CountryDetail', () => {
+describe("<CountryDetail>", () => {
+  beforeEach(() => {
     cy.mount(<CountryDetail />);
-    cy.contains('country');
+  });
+
+  it('Mounted CountryDetail', () => {
+    cy.contains('Back');
   })
+
+  it('Test back button', () => {
+    const currentUrl = cy.url();
+    cy.get('#back').click();
+    cy.url().should('not.eq', currentUrl);
+  });
+
+  it('Test if flag image has loaded', () => {
+    cy.get('#flag')
+    .should('be.visible')
+    .and('have.prop', 'naturalWidth')
+    .should('be.greaterThan', 0)
+  });
+
+  it('Test Border countries button changes url when clicked', () => {
+    const currentUrl1 = cy.url();
+    cy.get('#border1').click();
+    cy.url().should('not.eq', currentUrl1);
+    const currentUrl2 = cy.url();
+    cy.get('#border2').click();
+    cy.url().should('not.eq', currentUrl2);
+    const currentUrl3 = cy.url();
+    cy.get('#border3').click();
+    cy.url().should('not.eq', currentUrl3);
+  });
+
 });
