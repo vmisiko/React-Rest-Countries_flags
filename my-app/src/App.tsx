2878183e35aee5a304226b1c11d07000647531d4
victor misiko <victormisiko.vm@gmail.com>
2022-10-15 06:38:21 +0300

    Update: Add Header Component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,11 +3,13 @@ import {
   RouterProvider
 } from "react-router-dom";
 import router from './router';
+import Header from './SharedComponents/Header';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <RouterProvider router={router} />
     </div>
   );
---
+++ my-app/src/SharedComponents/Header.tsx
@@ -0,0 +1,11 @@
+import './header.scss';
+
+function header() {
+  return (
+    <div className="header">
+      <span className="header__title"> Where in the world</span>
+    </div>
+  )
+}
+
+export default header;
