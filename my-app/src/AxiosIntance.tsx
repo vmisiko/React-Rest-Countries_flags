0b12f964e9098053133b58ac577ae965ed62309d
victor misiko <victormisiko.vm@gmail.com>
2022-10-17 23:15:40 +0300

    Update: Remove interceptors from axios instance

--- my-app/src/AxiosIntance.tsx
+++ my-app/src/AxiosIntance.tsx
@@ -5,26 +5,5 @@ const axiosIntance = axios.create({
   baseURL: HOST_URL
 });
 
-axiosIntance.interceptors.response.use(
-  (response: AxiosResponse) => {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    return response;
-  },
-  (err: AxiosError) => {
-    if (err.response) {
-      const error = {
-        message: 'Server Error',
-        data: err.response.data,
-        statusCode: err.response.status
-      };
-      console.log(error);
-    } else {
-      const error = 'NetWork Error'
-      console.log(error);
-    }
-    return err;
-  }
-);
-
 export default axiosIntance;
 
